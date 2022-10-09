c4c9dbab5bfc7e2a652c09a4e600fb598a06e702
thuavu <thuvu2100@gmail.com>
2022-10-09 04:25:32 -0500

    Full website overhaul
    - Includes rotating logo
    - Sticky header
    - Clean up carousel
    - Overhaul portfolio style

---
+++ script.js
@@ -0,0 +1,61 @@
+/* For rotating the logo */
+var navimg = document.getElementById("img-logo");
+
+window.addEventListener("scroll", e => {
+    navimg.style.transform = "rotate("+ (window.pageYOffset/5) + "deg)";
+});
+
+/* For scroll down animation fade in and out */
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+/* For nav bar */
+let intro = document.getElementById("intro");
+let navbar = document.getElementById("main-nav");
+
+let navPos = navbar.getBoundingClientRect().top;
+
+window.addEventListener("scroll", e => {
+  let scrollPos = window.scrollY;
+  if (scrollPos > navPos) {
+    navbar.classList.add('sticky');
+    header.classList.add('navbarOffsetMargin');
+  } else {
+    navbar.classList.remove('sticky');
+    header.classList.remove('navbarOffsetMargin');
+  }
+});
+
+
+/* For slide show */
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
